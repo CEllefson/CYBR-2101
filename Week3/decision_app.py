8b66302409f5ed56926ff59aa82836550db400a9
CEllefson <ellefsonc@hocking.edu>
2025-11-06 09:18:14 -0500

    Improve print statement formatting with newlines
    Added newline characters to print statements for better readability.

--- Week3/decision_app.py
+++ Week3/decision_app.py
@@ -5,10 +5,10 @@ while True:
         'Please enter y/N:')
         user1 = input().lower()
         if user1 == 'y':
-            print("I'm glad you feel that way!")
+            print("I'm glad you feel that way!\n")
             break
         elif user1 == 'n':
-            print('I hope things turn around for you, reach out to the people around you and things can get better!')
+            print('I hope things turn around for you, reach out to the people around you and things can get better!\n')
             break
         
         else:
@@ -40,13 +40,46 @@ while True:
 while True:
     try:
         if user2 in [1, 2, 3]:
-            print("I'm sorry to hear things aren't going well, there are several resources available through the school, and your friends would likely be able to help!")
+            print("I'm sorry to hear things aren't going well, there are several resources available through the school, and your friends would likely be able to help!\n")
             break
         elif user2 in [4, 5, 6, 7]:
-            print("That's good but things could be better right? Try to think about things you can do to improve your situation and how the resources available can help. Reaching out can always improve your situation!")
+            print("That's good but things could be better right? Try to think about things you can do to improve your situation and how the resources available can help. Reaching out can always improve your situation!\n")
             break
         elif user2 in [8, 9, 10]:
-            print("That's great! Since things are going great for you, please think of ways to help others who aren't doing as well!")
+            print("That's great! Since things are going great for you, please think of ways to help others who aren't doing as well!\n")
+            break
+        else:
+            print('Invalid input, please try again')
+            
+    except ValueError:
+        print('Try again')
+
+        
+#input Validation Question 3, this one being numeric 1-10        
+while True:
+    try:
+        print('On a scale of 1-10, 1 being the worst and 10 being the best, how would you say things are going in life?\n'
+        'Please input 1-10:')
+        user3 = int(input())
+        #alternatively could use:
+        #if user2 in list(range(1,11)), this would be better for longer lists using range's (start, stop, step) paramenters, step is defaulted to 1
+        if user3 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            break
+        
+    except ValueError:
+        print('Invalid input, please try again')
+        continue
+#Question 3 input logic and output
+while True:
+    try:
+        if user3 in [1, 2, 3]:
+            print("I'm sorry thigns aren't going great right now, but things always improve with time and can benefit from help. Seek friends and family or try something new in life! There are plenty of resources available on the internet, local libraries, and even at school!\n")
+            break
+        elif user3 in [4, 5, 6, 7]:
+            print("There's nothing wrong with things being alright! Trying a new hobby or making time for friends and family can certainly make things go from good to great!\n")
+            break
+        elif user3 in [8, 9, 10]:
+            print("Amazing! I'm glad for you and happy things are going great! If you can help others feel the same way that would be a great thing!\n")
             break
         else:
             print('Invalid input, please try again')
