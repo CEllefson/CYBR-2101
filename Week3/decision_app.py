9672920a4ea2e490950b89c56d5cee2bc4c575c6
CEllefson <ellefsonc@hocking.edu>
2025-11-06 10:24:13 -0500

    Improve input prompts and validation messages

--- Week3/decision_app.py
+++ Week3/decision_app.py
@@ -1,8 +1,8 @@
-#Input Validation for first question, realistically should be turned into a defined function for several iterations.
+#Input Validation for first question, realistically should be turned into a defined function for several iterations, but will keep written out since length is not too long.
 while True:
     try:
         print('Are you enjoying school this year?\n'
-        'Please enter y/N:')
+        'Please enter y/N:\n')
         user1 = input().lower()
         if user1 == 'y':
             print("I'm glad you feel that way!\n")
@@ -23,7 +23,7 @@ while True:
 while True:
     try:
         print('On a scale of 1-10, 1 being the worst and 10 being the best, how would you rate you feelings on this year for school?\n'
-        'Please input 1-10:')
+        'Please input 1-10:\n')
         user2 = int(input())
         #alternatively could use:
         #if user2 in list(range(1,11)), this would be better for longer lists using range's (start, stop, step) paramenters, step is defaulted to 1
@@ -59,7 +59,7 @@ while True:
 while True:
     try:
         print('On a scale of 1-10, 1 being the worst and 10 being the best, how would you say things are going in life?\n'
-        'Please input 1-10:')
+        'Please input 1-10:\n')
         user3 = int(input())
         #alternatively could use:
         #if user2 in list(range(1,11)), this would be better for longer lists using range's (start, stop, step) paramenters, step is defaulted to 1
@@ -86,3 +86,85 @@ while True:
             
     except ValueError:
         print('Try again')
+        
+#Question 4
+import random
+
+rock = 'r'
+paper = 'p'
+scissor = 's'
+options = ['r', 'p', 's']
+
+
+#rock > scissor
+#rock < paper
+#paper < scissor
+
+#question 4 prompt with validation
+while True:
+    try:
+        print('Would you like to play Rock, Paper, Scissors? Please select the following options to continue:\n'
+                '1. Yes Please!\n'
+                '2. No Thanks!\n')
+        user = int(input())
+        if user == 1:
+            break
+        elif user == 2:
+            print('Have a Good Day!')
+            quit()
+        else:
+            if user != [1, 2]:
+                print('Invalid entry, please try a whole number from the list!')
+    except ValueError:
+        print('Invalid input, please try again')
+        continue
+#Rock, Paper, Scissors Game Loop    
+while True:
+    while True:
+        try:
+            print('Welcome to Rock/Paper/Scissors\n'
+                'Type R for Rock\n'
+                'Type P for Paper\n'
+                'Type S for Scissors\n'
+                'Please input your selection:')
+            player = input().lower()
+            if player in options:
+                break
+            else:
+                print('Invalid input, please try again')
+                        
+        except ValueError:
+            continue
+    computer = random.choice(options)
+
+    print(f'Your selection was: {player.upper()}, and The computer chose: {computer.upper()}')
+
+
+    if  (player == rock and computer == scissor) or \
+        (player == scissor and computer == paper) or \
+        (player == paper and computer == rock):
+        print('Player Wins:)')
+        
+    elif player == computer:
+        print('This game is a tie')
+        
+    else:
+        print('The Computer Wins!:(')
+        
+    while True:
+        try:
+            print('Would you like to play again? Please select the following options to continue:\n'
+                '1. Again Please\n'
+                '2. No Thanks')
+            user = int(input())
+            if user == 1:
+                break
+            elif user == 2:
+                print('Have a Good Day!')
+                quit()
+            else:
+                if user != [1, 2]:
+                    print('Invalid entry, please try a whole number from the list!')   
+                
+        except ValueError:
+            continue
